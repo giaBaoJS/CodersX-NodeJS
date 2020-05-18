4e6634ccdcb8eba619ab49d0d755a48f7d1c1162
Glitch (codersx-express-bai9-1) <none>
2020-05-18 03:58:56 +0000

    Lesson9-1

---
+++ controller/user.controller.js
@@ -0,0 +1,43 @@
+var db = require('../db');
+const shortid = require('shortid');
+module.exports.index = (request, response) => {
+    response.render("user/user", {
+      user: db.get('users').value()
+    });
+}
+
+module.exports.create = (req,res)=>{
+    res.render('user/userCreate.pug');
+}
+
+module.exports.postCreate = (req, res) => {
+  req.body.id=shortid.generate();
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
+};
+
+module.exports.delete = (req, res) => {
+  var id= req.params.id;
+  var todos= db.get('users').find({id :id}).value();
+  var c= db.get('users').indexOf(todos);
+  db.get('users').splice(c,1).write();
+  res.redirect('/users');
+};
+
+module.exports.getUpdate = (req, res) => {
+    var id=req.params.id;
+    var IdUser=db.get('users').find({id:id}).value();
+  console.log(IdUser);
+    res.render('user/updateUser.pug',{
+        user:IdUser
+    })
+};
+
+module.exports.postUpdate = (req, res) => {
+  var id= req.params.id;
+  db.get('users')
+        .find({ id:id })
+        .assign(req.body)
+        .write()
+    res.redirect('/users')
+};
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,50 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const shortid = require('shortid');
-var db = require('../db');
-router.get("/", (request, response) => {
-    response.render("books/index", {
-      books: db.get('books').value()
-    });
-});
+var controller = require('../controller/book.controller');
+router.get("/",controller.index);
 
-router.get('/create',(req,res)=>{
-    res.render('books/createBook.pug');
-});
+router.get('/create', controller.create);
 
-router.post("/create", (req, res) => {
-  req.body.id=shortid.generate();
-  db.get('books').push(req.body).write();
-  
-  res.redirect('/books');
+router.post("/create", controller.postCreate);
 
-});
-router.get("/:id/delete", (req, res) => {
-  var id= req.params.id;
-  var todos= db.get('books').find({id :id}).value();
-  var c= db.get('books').indexOf(todos);
-  db.get('books').splice(c,1).write();
-  res.redirect('/books');
+router.get("/:id/delete", controller.delete);
 
-});
+router.get('/:id/update',controller.getUpdate);
 
-router.get('/:id/update', (req, res) => {
-    var id=req.params.id;
-    var Idbook=db.get('books').find({id:id}).value();
-  console.log(Idbook);
-    res.render('books/update.pug',{
-        book:Idbook
-    })
-})
+router.post("/:id/update", controller.postUpdate);
 
-router.post("/:id/update", (req, res) => {
-  var id= req.params.id;
-  db.get('books')
-        .find({ id:id })
-        .assign(req.body)
-        .write()
-    res.redirect('/books')
-
-
-});
 module.exports = router;
