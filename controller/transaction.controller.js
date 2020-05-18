97e48d31b6ef45d5d8ce9e21e3c746fc24500b76
Glitch (codersx-express-bai9-2) <none>
2020-05-18 03:59:25 +0000

    Lesson 9-2

--- controller/transaction.controller.js
+++ controller/transaction.controller.js
@@ -19,10 +19,21 @@ module.exports.create = (req,res)=>{
 
 module.exports.postCreate = (req, res) => {
   req.body.id=shortid.generate();
+  var status=false;
   var id=req.body.id;
   var bookID = req.body.book;
   var userID = req.body.user;
-  var newTrans = {id, userID, bookID};
+  var newTrans = {id, userID, bookID ,status};
   db.get('transactions').push(newTrans).write();
   res.redirect('/transactions');
+};
+
+module.exports.complete = (req,res) => {
+   var id=req.params.id;
+    var IdTransaction=db.get('transactions').find({id:id}).value();
+  console.log(IdTransaction.status)
+  if(IdTransaction.status == false){
+    db.get('transactions').find({id:id}).assign(IdTransaction.status=true).write();
+     res.redirect('/transactions');
+  }
 };
